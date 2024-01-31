ba4c540da5c48a2b8b03f504936478a00868a250
Dale Maunder <dalemaunder@hotmail.com>
2024-01-31 14:34:50 +1100

    Added further package installation

--- prereqs.sh
+++ prereqs.sh
@@ -6,10 +6,22 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-echo "Installing packages"
-add-apt-repository universe
-apt-get update
+echo "Installing packages..."
+add-apt-repository universe -y
+apt-get update && apt-get upgrade -y
 apt-get install git -y
 apt-get install wget -y
 apt-get install curl -y
 apt-get install libfuse2 -y
+echo -e "\n"
+echo "Mandatory packages have been installed."
+
+echo -e "\n"
+read -rp "Would you like to install desirable optional packages? [y/n]" confirm 
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+	apt-get install ncdu -y
+	apt-get install htop -y
+    else
+	echo "Skipping non-mandatory packages."
+    fi
+
